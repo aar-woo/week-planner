df8a4ab9794d61e55b1ac367537a370372e1201e
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-13 16:55:23 -0700

    table

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 /* global data */
 /* exported data */
-
+var objtemp = {}
 var $ContainerModal = document.querySelector('.modal-container');
 var $openModal = document.querySelector('.add-btn');
 var $closeModal = document.querySelector('.submit-btn');
 var $addData = document.querySelector('.add-data');
+var $tbodyData = document.querySelector('.tbodyData')
 
 $openModal.addEventListener('click', showModal);
 $closeModal.addEventListener('click', closeModal);
@@ -19,12 +20,13 @@ function closeModal(event) {
 
 function submitData(event) {
   event.preventDefault();
-  var obj = {
+  objtemp = {
     time: $addData.elements.selectedTime.value,
     description: $addData.elements.description.value
   };
+  $tbodyData.appendChild(createEvent())
   var selectedDay = $addData.elements.days.value.toLowerCase();
-  data[selectedDay].push(obj);
+  data[selectedDay].push(objtemp);
   console.log('data', data);
 }
 $ContainerModal.addEventListener('submit', submitData);
@@ -34,12 +36,17 @@ $ContainerModal.addEventListener('submit', submitData);
   <td>wake up</td>
 </tr> */ }
 
-function createEvent(eventObj) {
+function createEvent() {
   var tableRow = document.createElement('tr');
   var timeData = document.createElement('td');
   var descriptionData = document.createElement('td');
-  timeData.textContent = event
-  tableRow.setAttribute('data-view', )
-  tableRow.setAttribute('id')
-  var
+  console.log("time",objtemp.time)
+  console.log("obj", objtemp)
+  timeData.innerText = objtemp.time
+  descriptionData.innerText = objtemp.description
+
+  tableRow.appendChild(timeData)
+  tableRow.appendChild(descriptionData)
+  objtemp = {}
+  return tableRow;
 }
