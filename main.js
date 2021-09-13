d9cf48d2b835fdccf0b21035c9d827620dbcd2c4
Jc Leon <leonjuan123@yahoo.com>
2021-09-13 14:34:11 -0700

    ljkasdf

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+/* global data */
+/* exported data */
+
 var $description = document.querySelector('#description');
 var $dayValue = document.querySelector('#days');
 var $timeValue = document.querySelector('#time');
 var $ContainerModal = document.querySelector('.modal-container');
 var $openModal = document.querySelector('.add-btn');
 var $closeModal = document.querySelector('.submit-btn');
+var $addData = document.querySelector('.add-data');
 
 $openModal.addEventListener('click', showModal);
 $closeModal.addEventListener('click', closeModal);
@@ -16,6 +20,10 @@ function closeModal(event) {
   $ContainerModal.setAttribute('class', 'modal-container overlay hidden');
 }
 
-console.log($description, $dayValue, $timeValue);
-
-$closeModal.addEventListener('submit');
+function submitData(event) {
+  event.preventDefault();
+  console.log('hi');
+  var selectedDay = $addData.elements.days.value;
+  console.log('hi', selectedDay);
+}
+$ContainerModal.addEventListener('submit', submitData);
