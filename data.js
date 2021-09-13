c1d5a3422d3cbb61e307fa2e8f9841fc25d823b7
Kenny Park <kennyparc@gmail.com>
2021-09-13 13:13:20 -0700

    fdsafaadddddds

---
+++ data.js
@@ -0,0 +1,20 @@
+var data = {
+  view: 'monday-view',
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: [],
+  sunday: []
+};
+
+function handleStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('weekly-data', dataJSON);
+}
+var previousDataJSON = localStorage.getItem('weekly-data');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+window.addEventListener('beforeunload', handleStorage);
--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-var $ContainerModal = document.querySelector('.modal-container')
-var $openModal = document.querySelector('.add-btn')
-var $closeModal = document.querySelector('.submit-btn')
+var $description = document.querySelector('#description');
+var $dayValue = document.querySelector('#days');
+var $timeValue = document.querySelector('#time');
+var $ContainerModal = document.querySelector('.modal-container');
+var $openModal = document.querySelector('.add-btn');
+var $closeModal = document.querySelector('.submit-btn');
 
-$openModal.addEventListener('click',showModal);
-$closeModal.addEventListener('click', hideModal);
+$openModal.addEventListener('click', showModal);
+$closeModal.addEventListener('click', closeModal);
 
-
-function showModal(event){
-  $ContainerModal.setAttribute('class','modal-container overlay')
+function showModal(event) {
+  $ContainerModal.setAttribute('class', 'modal-container overlay');
 }
 
 function closeModal(event) {
-  $ContainerModal.setAttribute('class', 'modal-container overlay hidden')
+  $ContainerModal.setAttribute('class', 'modal-container overlay hidden');
 }
+
+console.log($description, $dayValue, $timeValue);
+
+$closeModal.addEventListener('submit');
