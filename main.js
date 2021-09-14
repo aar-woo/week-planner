0896614552f2d0c0f887db01261d5da2fa5eeef7
Kenny Park <kennyparc@gmail.com>
2021-09-13 17:13:39 -0700

    click each day button data view

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /* global data */
 /* exported data */
-var objtemp = {}
+var objtemp = {};
 var $ContainerModal = document.querySelector('.modal-container');
 var $openModal = document.querySelector('.add-btn');
 var $closeModal = document.querySelector('.submit-btn');
 var $addData = document.querySelector('.add-data');
-var $tbodyData = document.querySelector('.tbodyData')
+var $tbodyData = document.querySelector('.tbodyData');
+var $daysSelected = document.querySelector('#days-selected');
 
 $openModal.addEventListener('click', showModal);
 $closeModal.addEventListener('click', closeModal);
@@ -24,29 +25,33 @@ function submitData(event) {
     time: $addData.elements.selectedTime.value,
     description: $addData.elements.description.value
   };
-  $tbodyData.appendChild(createEvent())
+  $tbodyData.appendChild(createEvent());
   var selectedDay = $addData.elements.days.value.toLowerCase();
   data[selectedDay].push(objtemp);
   console.log('data', data);
 }
 $ContainerModal.addEventListener('submit', submitData);
 
-{ /* <tr>
-  <td>11:00am</td>
-  <td>wake up</td>
-</tr> */ }
-
 function createEvent() {
+
   var tableRow = document.createElement('tr');
   var timeData = document.createElement('td');
   var descriptionData = document.createElement('td');
-  console.log("time",objtemp.time)
-  console.log("obj", objtemp)
-  timeData.innerText = objtemp.time
-  descriptionData.innerText = objtemp.description
+  console.log('time', objtemp.time);
+  console.log('obj', objtemp);
+  timeData.innerText = objtemp.time;
+  descriptionData.innerText = objtemp.description;
 
-  tableRow.appendChild(timeData)
-  tableRow.appendChild(descriptionData)
-  objtemp = {}
+  tableRow.appendChild(timeData);
+  tableRow.appendChild(descriptionData);
+  objtemp = {};
   return tableRow;
+}
+
+$daysSelected.addEventListener('click', switchView);
+
+function switchView(event) {
+  console.log(event.target.getAttribute('data-view'));
+  data.view = event.target.getAttribute('data-view');
+  console.log('data.view', data.view);
 }
